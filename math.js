d7cc8bfe7a2830755a1f8c8f3c9580af5fe1af34
doggbmx <manuelorticosta@gmail.com>
2023-07-13 11:15:13 -0400

    first commit

---
+++ math.js
@@ -0,0 +1,30 @@
+console.group("Cuadrados");
+const lado = 5;
+const perimetroCuadrado = lado * 4;
+
+const areaCuardrado = lado * lado;
+
+console.log({
+  lado,
+  perimetroCuadrado,
+  areaCuardrado,
+});
+console.groupEnd();
+
+console.group("Triangulos");
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTrianguloBase = 4;
+const alturaTriangulo = 5.5;
+
+const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + ladoTrianguloBase;
+const areaTriangulo = (ladoTrianguloBase * alturaTriangulo) / 2;
+
+console.log({
+  ladoTriangulo1,
+  ladoTriangulo2,
+  ladoTrianguloBase,
+  perimetroTriangulo,
+  areaTriangulo,
+});
+console.groupEnd();
