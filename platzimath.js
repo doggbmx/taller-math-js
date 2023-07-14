3349bf35ed5df258fd05b811c80905a2861239ff
doggbmx <manuelorticosta@gmail.com>
2023-07-14 14:38:31 -0400

    end class 13

--- platzimath.js
+++ platzimath.js
@@ -1,8 +1,23 @@
 function calcularPromedio(array) {
   // sumar todos los elementos del array y dividir por la cantidad de elementos
+  //   let sumaElementos;
+  //   for (let i = 0; i < array.length; i++) {
+  //     sumaElementos += array[i];
+  //   }
+
+  // ahora intentaremos hacerlo con metodos de array
   let sumaElementos;
-  for (let i = 0; i < array.length; i++) {
-    sumaElementos += array[i];
-  }
+  // .reduce() reduce todos los elementos de una lista a un solo elemento
+  sumaElementos = array.reduce((valorAcumulado, nuevoValor) => {
+    return valorAcumulado + nuevoValor;
+  });
+
   return sumaElementos / array.length;
+}
+
+function arrayIsOdd(array) {
+  if (array.length % 2) {
+    return true;
+  }
+  return false;
 }
