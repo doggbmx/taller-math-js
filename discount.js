0831db22cc0ed41389b059d4fad150528742393d
doggbmx <manuelorticosta@gmail.com>
2023-07-14 13:23:50 -0400

    end class 8

---
+++ discount.js
@@ -0,0 +1,57 @@
+const inputPrice = document.getElementById("InputPrice");
+// const coupon = document.getElementById("coupon");
+const resultP = document.getElementById("ResultP");
+
+const selectCupones = document.getElementById("selectCoupons");
+const discounts = {
+  diaDeLaIndependencia: 15,
+  blackFriday: 30,
+  diaDeLaMadre: 25,
+  diaDelPadre: 20,
+  navidad: 10,
+};
+
+for (cupon in discounts) {
+  const option = document.createElement("option");
+  option.value = discounts[cupon];
+  option.textContent = cupon;
+  selectCupones.appendChild(option);
+}
+function getDiscountPercentage() {
+  const couponValue = coupon.value;
+  const discountPercentage = discounts[couponValue];
+  return discountPercentage;
+}
+
+function calcularDescuento(precio, descuento) {
+  if (descuento >= 100) {
+    alert("El descuento no puede ser mayor o igual a 100%");
+    return;
+  }
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+  return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+  const priceValue = Number(inputPrice.value);
+  const discountValue = selectCupones.value;
+
+  if (!priceValue || !discountValue) {
+    alert("Por favor ingrese los valores");
+    return;
+  }
+
+  const precioConDescuento = calcularDescuento(priceValue, discountValue);
+
+  if (!precioConDescuento) {
+    return;
+  }
+
+  resultP.innerText = `El precio con descuento son: $${precioConDescuento}`;
+}
+
+document.getElementById("buttonCalcular").addEventListener("click", (event) => {
+  event.preventDefault();
+  onClickButtonPriceDiscount();
+});
