6ec3b285dc0693c0a20e33a867c0e945e1e891cb
doggbmx <manuelorticosta@gmail.com>
2023-07-17 21:36:48 -0400

    end class 24

--- analisis.js
+++ analisis.js
@@ -15,3 +15,30 @@ function medianaPorPersona(nombrePersona) {
 
   return medianaSalarios;
 }
+
+function proyeccionPorPersona(nombrePersona) {
+  const trabajosDePersona = encontrarPersona(nombrePersona).trabajos;
+
+  let porcentajesDeCrecimiento = [];
+
+  for (let i = 1; i < trabajosDePersona.length; i++) {
+    const salarioActual = trabajosDePersona[i].salario;
+    const salarioAnterior = trabajosDePersona[i - 1].salario;
+
+    const crecimiento = salarioActual - salarioAnterior;
+    const porcentajeCrecimiento = crecimiento / salarioAnterior;
+    console.log(porcentajeCrecimiento);
+    porcentajesDeCrecimiento.push(porcentajeCrecimiento);
+  }
+  console.log(`crecimiento => ${porcentajesDeCrecimiento}`);
+
+  const medianaPorcentajeCrecimiento = PlatziMath.calcularMediana(
+    porcentajesDeCrecimiento
+  );
+
+  const ultimoSalario = trabajosDePersona[trabajosDePersona.length - 1].salario;
+
+  const aumento = ultimoSalario * medianaPorcentajeCrecimiento;
+  const nuevoSalario = ultimoSalario + aumento;
+  return nuevoSalario;
+}
